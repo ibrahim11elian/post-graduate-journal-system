ff356923ed6f667f6da73d30d3051ec3cf69820a
Ibrahim Ahmed Abdelnaby <58626527+ibrahim11elian@users.noreply.github.com>
2023-09-10 11:30:30 +0300

    adding more space to research title to accept large titles

--- back-end/migrations/sqls/20230409014655-research-table-up.sql
+++ back-end/migrations/sqls/20230409014655-research-table-up.sql
@@ -1,6 +1,6 @@
 create table research (
 	id SERIAL PRIMARY KEY NOT NULL,
-	research_title VARCHAR(200) NOT NULL,
+	research_title VARCHAR(300) NOT NULL,
 	research_pdf VARCHAR(300) NOT NULL,
 	research_summary VARCHAR(300),
 	research_summary_ar VARCHAR(300),
