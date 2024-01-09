f60ad75b616094bfb4e7f5060c1a334963183873
ibrahim11elian <ibrahim11elian@gmail.com>
2024-01-09 22:38:46 +0200

    update structure

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { UserContextProvider } from "./utilities/context";
+import { UserContextProvider } from "./context/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
